2098d9d2febdd7a6dcc125f4ae6704a4f19bbce5
trooperdll <trooper@hatesje.ws>
2025-02-27 21:11:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('fileForm');
-    const fileInput = document.getElementById('fileInput');
-    const descriptionInput = document.getElementById('descriptionInput');
-    const fileList = document.getElementById('files');
+.star {
+    position: absolute;
+    border-radius: 50%;
+    background-color: white;
+    opacity: 0.5;
+    animation: blink 3s infinite alternate;
+}
 
-    let files = [];
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const file = fileInput.files[0];
-        const description = descriptionInput.value;
-
-        if (file && file.type === "text/plain") {
-            const fileURL = URL.createObjectURL(file);
-            files.push({ file: fileURL, description, name: file.name });
-
-            fileInput.value = '';
-            descriptionInput.value = '';
-
-            updateFileList();
-        } else {
-            alert("Please upload a valid text file.");
-        }
-    });
-
-    function updateFileList() {
-        fileList.innerHTML = '';
-
-        files.forEach((item) => {
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            a.href = item.file;
-            a.target = "_blank";
-            a.textContent = item.name;
-            li.appendChild(a);
-
-            if (item.description) {
-                const p = document.createElement('p');
-                p.textContent = item.description;
-                li.appendChild(p);
-            }
-
-            fileList.appendChild(li);
-        });
+@keyframes blink {
+    0% {
+        opacity: 0.2;
     }
-});
+    100% {
+        opacity: 1;
+    }
+}
